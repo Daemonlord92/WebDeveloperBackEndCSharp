041922f29ef8a8e4b6b0ca0b647a940d0f74aafe
Matthew Martin <matthew-martin@programmer.net>
2022-03-31 13:47:29 -0400

    Deleted the Privicy Route, Updated so the View pages will not show Create New link to people not signed in

--- WebDeveloperVC/Controllers/HomeController.cs
+++ WebDeveloperVC/Controllers/HomeController.cs
@@ -18,10 +18,6 @@ namespace WebDeveloperVC.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
